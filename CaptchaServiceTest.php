d4682da5f5b8c61c87861df23b3997c2f953cc97
kamol <kamolcu@gmail.com>
2014-05-24 15:42:48 +0700

    add case 2527

--- CaptchaServiceTest.php
+++ CaptchaServiceTest.php
@@ -17,4 +17,19 @@ class CaptchaServiceTest extends PHPUnit_Framework_TestCase{
         $this->assertSame('2', $obj->getRightOperand());
         $this->assertSame('+', $obj->getOperator());
     }
+    public function testGetCaptcha_2527(){
+        $stubRandom = $this->getMock('RandomGenerator');
+        $stubRandom->expects($this->once())->method('getPattern')->will(
+                $this->returnValue(2));
+        $stubRandom->expects($this->exactly(2))->method('getOperand')->will(
+                $this->onConsecutiveCalls(5, 7));
+        $stubRandom->expects($this->once())->method('getOperator')->will(
+                $this->returnValue(2));
+        $cs = new CaptchaService($stubRandom);
+        $obj = $cs->getCaptcha();
+        $this->assertSame('35', $obj->getResult());
+        $this->assertSame('5', $obj->getLeftOperand());
+        $this->assertSame('Seven', $obj->getRightOperand());
+        $this->assertSame('*', $obj->getOperator());
+    }
 }
